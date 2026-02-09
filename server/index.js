6ee84d1488cd576f826e8c96a1338124aa5db6ee
CallumDotJ <162898463+CallumDotJ@users.noreply.github.com>
2026-02-09 17:43:08 +0000

    Repo change.

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-// Router Routes -- saved
+// Router Routes -- saved // CHANGE
 app.use("/api/openai", openaiRoutes);
 
 const PORT = process.env.PORT || 5000;
